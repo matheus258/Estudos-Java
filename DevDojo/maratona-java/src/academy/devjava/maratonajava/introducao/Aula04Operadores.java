5dccca6a5f40b08ffcead8fb6d1cc54301df81e5
matheus258 <matheusmarquesaraujo53@gmail.com>
2024-11-02 21:38:38 -0300

    Logicos AND e OR

--- DevDojo/maratona-java/src/academy/devjava/maratonajava/introducao/Aula04Operadores.java
+++ DevDojo/maratona-java/src/academy/devjava/maratonajava/introducao/Aula04Operadores.java
@@ -5,5 +5,49 @@ public class Aula04Operadores {
         int numero01 = 10;
         int numero02 = 20;
 
+        System.out.println("SOMA - Valor: "+ (numero01 + numero02));
+
+        // Exemplo de como usar o operador % (resto)
+        int numero03 = 20;
+
+        if (numero03 % 2 == 0 ){
+            System.out.println("O numero é par.");
+        } else {
+            System.out.println("O numero é ímpar");
+        }
+
+        // < > <= >=
+
+        if(numero03 > numero02 ){
+            System.out.println("O número "+ numero03+ " é maior que o número "+ numero02);
+        } else if (numero03 < numero02) {
+            System.out.println("O número "+ numero03+ " é menor que o número "+ numero02);
+        } else if (numero03 == numero02) {
+            System.out.println("O número "+ numero03+ " é igual ao número "+ numero02);
+        }
+
+        //  Lógicos AND, todas as condicoes precisam ser true
+
+        if(numero03 > numero01 & numero03 != numero02 ){
+            System.out.println("\nO número "+numero03+" é maior que o número "+numero01+" e diferente do número "+ numero02);
+        } else if (numero03 > numero01 & numero03 == numero02) {
+            System.out.println("\nO número "+numero03+" é maior que o número "+numero01+" e igual ao número "+ numero02);
+        }
+
+        // Lógicos AND, todas as condicoes precisam ser true
+
+        String senhaFesta = "Papagaio";
+        String segundaSenhaFesta = "Tatu";
+        String entradaSenha = "Papagaio";
+
+        if(entradaSenha == senhaFesta || entradaSenha == segundaSenhaFesta){
+            System.out.println("\nSua entrada a festa foi altorizada, \nSejá bem-vindo!");
+
+        }
+
+
     }
+
+
+
 }

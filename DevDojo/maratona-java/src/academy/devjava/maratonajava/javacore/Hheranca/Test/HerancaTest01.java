c27e4e0822b44a69f618056a991b7525ef14ecb4
matheus258 <matheusmarquesaraujo53@gmail.com>
2024-11-08 23:43:52 -0300

    Herança

---
+++ DevDojo/maratona-java/src/academy/devjava/maratonajava/javacore/Hheranca/Test/HerancaTest01.java
@@ -0,0 +1,30 @@
+package academy.devjava.maratonajava.javacore.Hheranca.Test;
+
+import academy.devjava.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devjava.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devjava.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Miriam alves de Melo");
+        endereco.setCep("012345-289");
+
+        Endereco endereco1 = new Endereco();
+        endereco1.setRua("One piece Grand Line");
+        endereco1.setCep("0056478944");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Hinata");
+        pessoa.setCpf("75889468563");
+        pessoa.setEndereco(endereco);
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Monkey D. Luffy");
+        funcionario.setCpf("8000000");
+        funcionario.setEndereco(endereco1);
+
+        pessoa.imprime();
+        funcionario.imprime();
+    }
+}
